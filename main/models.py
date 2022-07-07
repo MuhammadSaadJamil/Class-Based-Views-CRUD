a41f08ee26840afdb94209db8e1c6ff2fc756e2c
Dev <saad.jamil@devsinc.com>
2022-07-07 15:29:16 +0500

    Initial commit

---
+++ main/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+
+class Object(models.Model):
+    name = models.CharField(max_length=25, blank=True, null=True)
+    quantity = models.IntegerField(default=0)
---
+++ main/views.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+from .models import Object
+
+
+class ListObjectView(ListView):
+    model = Object
+    template_name = 'index.html'
+    context_object_name = "objects"
+
+
+list_objects = ListObjectView.as_view()
+
+
+class AddObject(CreateView):
+    model = Object
+    fields = "__all__"
+    template_name = 'add.html'
+    success_url = reverse_lazy('list')
+
+
+add_object = AddObject.as_view()
+
+
+class UpdateObject(UpdateView):
+    model = Object
+    fields = '__all__'
+    template_name = 'add.html'
+    success_url = reverse_lazy("list")
+
+
+update_object = UpdateObject.as_view()
+
+
+class DeleteObject(LoginRequiredMixin, DeleteView):
+    model = Object
+    success_url = reverse_lazy("list")
+    template_name = 'delete.html'
+
+
+delete_object = DeleteObject.as_view()
