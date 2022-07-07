e1189257621a7e9cc6e119cc00568fb348ab7e56
Dev <saad.jamil@devsinc.com>
2022-07-07 16:50:30 +0500

    Updated Styling

--- main/models.py
+++ main/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Object(models.Model):
     name = models.CharField(max_length=25, blank=True, null=True)
     quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
